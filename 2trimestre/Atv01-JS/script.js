467b974b2013fa8710f3b927f5b4d25db0386f2e
Vitor Klein <vitorhklein2@gmail.com>
2021-07-30 16:48:33 -0300

    atv01-js

---
+++ 2trimestre/Atv01-JS/script.js
@@ -0,0 +1,16 @@
+//alert("Bem-Vindo")
+
+a = 30
+b = 5
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+document.write("<br>", "O Resultado da SOMA é: " + c);
+
+document.write("<br>", "O Resultado da SUBTRAÇÃO é: " + d);
+
+document.write("<br>", "O Resultado da MULTIPLICAÇÃO é: " + e);
+
+document.write("<br>", "O Resultado da DIVISÃO é: " + f);
+document.querySelector(".res").innerHTML = "Vitor Klein nº26 3ºinfo A.";
